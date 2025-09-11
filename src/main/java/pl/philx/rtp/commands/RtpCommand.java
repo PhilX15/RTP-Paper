3b2cc7a27d104ad43aa0033e1c51b4813a5a50f5
FKalin15 <filipkal55@gmail.com>
2025-09-11 17:56:34 +0200

    Add dangerous random teleport

--- src/main/java/pl/philx/rtp/commands/RtpCommand.java
+++ src/main/java/pl/philx/rtp/commands/RtpCommand.java
@@ -6,6 +6,8 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NullMarked;
 
+import java.util.Random;
+
 @NullMarked
 public class RtpCommand implements BasicCommand {
     @Override
@@ -14,6 +16,18 @@ public class RtpCommand implements BasicCommand {
            return;
         }
 
-        sender.teleport(new Location(sender.getWorld(), 0, 100, 0));
+        Random randomNumberGenerator = new Random();
+        final int TELEPORT_RANGE = 500;
+
+        Location playerLocation = sender.getLocation();
+        final int randomX = playerLocation.getBlockX() + getRandomCoordinate(randomNumberGenerator, TELEPORT_RANGE);
+        final int randomZ = playerLocation.getBlockZ() + getRandomCoordinate(randomNumberGenerator, TELEPORT_RANGE);
+        Location randomLocation = new Location(sender.getWorld(), randomX, 80, randomZ);
+
+        sender.teleport(randomLocation);
+    }
+
+    private int getRandomCoordinate(Random rng, int range) {
+        return rng.nextInt(2 * range) - range;
     }
 }
