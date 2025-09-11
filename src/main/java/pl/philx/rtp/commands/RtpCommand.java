55ca4f2a072d3ecf4d867e821ad9c2281ef7bb10
FKalin15 <filipkal55@gmail.com>
2025-09-11 19:53:54 +0200

    Make /rtp command more safe

--- src/main/java/pl/philx/rtp/commands/RtpCommand.java
+++ src/main/java/pl/philx/rtp/commands/RtpCommand.java
@@ -2,7 +2,9 @@ package pl.philx.rtp.commands;
 
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NullMarked;
 
@@ -17,19 +19,43 @@ public class RtpCommand implements BasicCommand {
            return;
         }
 
+        sender.sendRichMessage("<yellow>Teleporting...</yellow>");
+
         Location playerLocation = sender.getLocation();
+        Location destination = null;
         Random randomNumberGenerator = new Random();
-        int teleportRange = 500;
-        final int randomX = playerLocation.getBlockX() + getRandomCoordinate(randomNumberGenerator, teleportRange);
-        final int randomZ = playerLocation.getBlockZ() + getRandomCoordinate(randomNumberGenerator, teleportRange);
-
+        int teleportRange = 50;
         int oceanLevel = 63;
-        Location randomLocation = new Location(sender.getWorld(), randomX, oceanLevel, randomZ);
+        int attempts = 10;
+        while (attempts > 0) {
+            final int randomX = playerLocation.getBlockX() + getRandomCoordinate(randomNumberGenerator, teleportRange);
+            final int randomZ = playerLocation.getBlockZ() + getRandomCoordinate(randomNumberGenerator, teleportRange);
 
-        sender.teleport(randomLocation);
+            Location randomLocation = new Location(sender.getWorld(), randomX, oceanLevel, randomZ);
+
+            if (isSafeLocation(randomLocation)) {
+                destination = randomLocation;
+                break;
+            }
+
+            attempts--;
+        }
+
+        if (destination == null) {
+            sender.sendRichMessage("<red>Unable to find a self place to teleport</red>");
+            return;
+        }
+
+        sender.sendRichMessage("<green>Successful teleport</green>");
+        sender.teleport(destination);
     }
 
     private int getRandomCoordinate(Random rng, int range) {
         return rng.nextInt(2 * range) - range;
+    }
+
+    private boolean isSafeLocation(Location location) {
+        Location blockAbove = location.add(0, 1, 0);
+        return (location.getBlock().getType() == Material.AIR && blockAbove.getBlock().getType() == Material.AIR);
     }
 }
