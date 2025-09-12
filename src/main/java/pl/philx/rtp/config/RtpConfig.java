1f163f8aaeca5d7b505d45408bce08fbb2a69d88
FKalin15 <filipkal55@gmail.com>
2025-09-12 11:46:56 +0200

    Add leaves to block blacklist

--- src/main/java/pl/philx/rtp/config/RtpConfig.java
+++ src/main/java/pl/philx/rtp/config/RtpConfig.java
@@ -9,7 +9,7 @@ public class RtpConfig {
     private final int teleportRange;
     private final int oceanLevel;
     private final int teleportAttempts;
-    private final List<Material> blockBlacklist = List.of(Material.AIR, Material.MAGMA_BLOCK, Material.LAVA, Material.WATER, Material.BEDROCK, Material.POWDER_SNOW, Material.CAMPFIRE, Material.FIRE, Material.BIG_DRIPLEAF, Material.SOUL_CAMPFIRE);
+    private final List<Material> blockBlacklist = List.of(Material.AIR, Material.MAGMA_BLOCK, Material.LAVA, Material.WATER, Material.BEDROCK, Material.POWDER_SNOW, Material.CAMPFIRE, Material.FIRE, Material.BIG_DRIPLEAF, Material.SOUL_CAMPFIRE, Material.ACACIA_LEAVES, Material.AZALEA_LEAVES, Material.BIRCH_LEAVES, Material.CHERRY_LEAVES, Material.FLOWERING_AZALEA_LEAVES, Material.JUNGLE_LEAVES, Material.MANGROVE_LEAVES, Material.OAK_LEAVES, Material.DARK_OAK_LEAVES, Material.PALE_OAK_LEAVES, Material.SPRUCE_LEAVES);
     private final List<Biome> biomeBlacklist = List.of(Biome.OCEAN, Biome.COLD_OCEAN, Biome.DEEP_COLD_OCEAN, Biome.DEEP_OCEAN, Biome.DEEP_FROZEN_OCEAN, Biome.DEEP_LUKEWARM_OCEAN, Biome.LUKEWARM_OCEAN, Biome.WARM_OCEAN);
 
     public RtpConfig(int teleportRange, int oceanLevel, int teleportAttempts) {
